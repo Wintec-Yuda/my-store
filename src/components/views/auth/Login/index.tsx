34aa16500c9175175181aa4bdf057d75c724ff48
Wintec-Yuda <alamasta004@gmail.com>
2024-04-07 11:20:20 +0700

    instance fetch axios

--- src/components/views/auth/Login/index.tsx
+++ src/components/views/auth/Login/index.tsx
@@ -5,6 +5,7 @@ import { FormEvent, useState } from "react";
 import { signIn } from "next-auth/react";
 import Input from "@/components/ui/Input";
 import Button from "@/components/ui/Button";
+import AuthLayout from "@/components/layouts/AuthLayout";
 
 const LoginView = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -43,28 +44,21 @@ const LoginView = () => {
   };
 
   return (
-    <div className={styles.login}>
-      <h1 className={styles.login__title}>Login</h1>
-      {error && <p className={styles.login__error}>{error}</p>}
-      <div className={styles.login__form}>
-        <form onSubmit={handleSubmit}>
-          <Input label="Email" type="email" name="email" />
-          <Input label="Password" type="password" name="password" />
-          <Button type="submit" className={styles.login__form__button}>
-            {isLoading ? "Loading..." : "Login"}
-          </Button>
-        </form>
-        <hr className={styles.login__form__devider} />
-        <div className={styles.login__form__other}>
-          <Button type="button" onClick={() => signIn("google", { callbackUrl, redirect: false })} className={styles.login__form__other__button}>
-            <i className="bx bxl-google" /> Login with google
-          </Button>
-        </div>
+    <AuthLayout title="Login" error={error} link="/auth/register" linkText="Don't have an account?">
+      <form onSubmit={handleSubmit}>
+        <Input label="Email" type="email" name="email" />
+        <Input label="Password" type="password" name="password" />
+        <Button type="submit" className={styles.login__button}>
+          {isLoading ? "Loading..." : "Login"}
+        </Button>
+      </form>
+      <hr className={styles.login__devider} />
+      <div className={styles.login__other}>
+        <Button type="button" onClick={() => signIn("google", { callbackUrl, redirect: false })} className={styles.login__other__button}>
+          <i className="bx bxl-google" /> Login with google
+        </Button>
       </div>
-      <p className={styles.login__link}>
-        Don{"'"}t have an account? Sign up <Link href="/auth/register">here</Link>
-      </p>
-    </div>
+    </AuthLayout>
   );
 };
 
--- src/components/views/auth/Register/index.tsx
+++ src/components/views/auth/Register/index.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
 import Input from "@/components/ui/Input";
 import Button from "@/components/ui/Button";
+import authServices from "@/services/auth";
+import AuthLayout from "@/components/layouts/AuthLayout";
 
 const RegisterView = () => {
   const { push } = useRouter();
@@ -24,13 +26,7 @@ const RegisterView = () => {
       password: form.password.value,
     };
 
-    const result = await fetch("/api/user/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    const result = await authServices.registerAccount(data);
 
     if (result.status === 200) {
       setIsLoading(false);
@@ -43,22 +39,17 @@ const RegisterView = () => {
   };
 
   return (
-    <div className={styles.register}>
-      <h1 className={styles.register__title}>Register</h1>
-      {error && <p className={styles.register__error}>{error}</p>}
-      <div className={styles.register__form}>
-        <form onSubmit={handleSubmit}>
-          <Input label="Email" type="email" name="email" />
-          <Input label="Fullname" type="text" name="fullname" />
-          <Input label="Phone" type="number" name="phone" />
-          <Input label="Password" type="password" name="password" />
-          <Button type="submit" className={styles.register__form__button}>{isLoading ? "Loading..." : "Register"}</Button>
-        </form>
-      </div>
-      <p className={styles.register__link}>
-        Have an account? Sign in <Link href="/auth/login">here</Link>
-      </p>
-    </div>
+    <AuthLayout title="Register" error={error} link="/auth/login" linkText="Already have an account?">
+      <form onSubmit={handleSubmit}>
+        <Input label="Email" type="email" name="email" />
+        <Input label="Fullname" type="text" name="fullname" />
+        <Input label="Phone" type="number" name="phone" />
+        <Input label="Password" type="password" name="password" />
+        <Button type="submit" className={styles.register__button}>
+          {isLoading ? "Loading..." : "Register"}
+        </Button>
+      </form>
+    </AuthLayout>
   );
 };
 

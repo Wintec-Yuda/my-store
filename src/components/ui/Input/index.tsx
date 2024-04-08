4bfeca26776a10b85f05b960e1a1fa14cea19e20
Wintec-Yuda <alamasta004@gmail.com>
2024-04-08 15:50:44 +0700

    update & delete users

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -5,15 +5,17 @@ type Proptypes = {
   name: string;
   type: string;
   placeholder?: string;
+  defaultValue?: string;
+  disabled?: boolean;
 };
 
 const Input = (props: Proptypes) => {
-  const { label, name, type, placeholder } = props;
+  const { label, name, type, placeholder, defaultValue, disabled } = props;
 
   return (
     <div className={styles.container}>
       {label && <label htmlFor={name}>{label}</label>}
-      <input type={type} name={name} id={name} placeholder={placeholder} className={styles.container__input} />
+      <input type={type} name={name} id={name} placeholder={placeholder} className={styles.container__input} defaultValue={defaultValue} disabled={disabled} />
     </div>
   );
 };
--- src/components/views/admin/Users/index.tsx
+++ src/components/views/admin/Users/index.tsx
@@ -1,6 +1,9 @@
 import Button from "@/components/ui/Button";
 import styles from "./Users.module.scss";
 import AdminLayout from "@/components/layouts/AdminLayout";
+import { useEffect, useState } from "react";
+import ModalUpdateUser from "./ModalUpdateUser";
+import ModalDeleteUser from "./ModalDeleteUser";
 
 type Proptypes = {
   users: any;
@@ -8,42 +11,57 @@ type Proptypes = {
 
 const UserAdminView = (props: Proptypes) => {
   const { users } = props;
+  const [updatedUser, setUpdatedUser] = useState<any>({});
+  const [deletedUser, setDeletedUser] = useState<any>({});
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    setUsersData(users);
+  }, [users]);
 
   return (
-    <AdminLayout>
-      <div className={styles.users}>
-        <h1>User Management</h1>
-        <table className={styles.users__table}>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Fullname</th>
-              <th>Emain</th>
-              <th>Phone</th>
-              <th>Role</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user: any, index: number) => (
-              <tr key={user.id}>
-                <td>{index + 1}</td>
-                <td>{user.fullname}</td>
-                <td>{user.email}</td>
-                <td>{user.phone}</td>
-                <td>{user.role}</td>
-                <td>
-                  <div className={styles.users__table__action}>
-                  <Button type="button">Update</Button>
-                  <Button type="button">Delete</Button>
-                  </div>
-                </td>
+    <>
+      <AdminLayout>
+        <div className={styles.users}>
+          <h1>User Management</h1>
+          <table className={styles.users__table}>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Fullname</th>
+                <th>Emain</th>
+                <th>Phone</th>
+                <th>Role</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </AdminLayout>
+            </thead>
+            <tbody>
+              {usersData.map((user: any, index: number) => (
+                <tr key={user.id}>
+                  <td>{index + 1}</td>
+                  <td>{user.fullname}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.role}</td>
+                  <td>
+                    <div className={styles.users__table__action}>
+                      <Button type="button" className={styles.users__table__action__edit} onClick={() => setUpdatedUser(user)}>
+                        <i className="bx bxs-edit" />
+                      </Button>
+                      <Button type="button" className={styles.users__table__action__delete} onClick={() => setDeletedUser(user)}>
+                        <i className="bx bxs-trash" />
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </AdminLayout>
+      {Object.keys(updatedUser).length > 0 && <ModalUpdateUser updatedUser={updatedUser} setUpdatedUser={setUpdatedUser} setUsersData={setUsersData} />}
+      {Object.keys(deletedUser).length > 0 && <ModalDeleteUser deletedUser={deletedUser} setDeletedUser={setDeletedUser} setUsersData={setUsersData} />}
+    </>
   );
 };
 
